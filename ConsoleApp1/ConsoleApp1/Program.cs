607ff2a675673a7cf2f08df24f36356d525d252a
Luluie <ryouya.nakai0103@gmail.com>
2019-11-28 02:55:38 +0900

    課題の４をＣ＃で再開

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+public class Hello
+{
+    public static void Main()
+    {
+        int[] vs = { };
+
+        Console.WriteLine("Xの値を入力");
+        int x = Console.Read();
+        Console.WriteLine("Aの値を入力");
+        int a = Console.Read();
+        Console.WriteLine("Nの値を入力");
+        int n = Console.Read();
+        
+        vs = new int[n];
+        for(int i = 0; i < n; i++)
+        {
+            vs[i]=i;
+        }
+
+        Console.Write(Horner(x,n,a));
+    }
+
+    public  static double Horner(int x,int n,int a)
+    {
+        //int f = 0;
+
+
+        int[] vs = new int[n+1];
+        for (int i = 0; i < n; i++)
+        {
+            vs[i] = i;
+        }
+
+        double f = vs[n];
+        for (int i = n - 1; i >= 0; i--)
+        {
+            f = f * x + vs[i];
+        }
+        return f;
+
+        //      for (int i = n-1; i >= 0; i--)
+        //f = f* x + vs[i];
+        //      return f;
+    }
+}
