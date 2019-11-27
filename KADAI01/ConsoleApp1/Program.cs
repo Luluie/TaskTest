560448db1261fd629796389f351d941f2047da28
Luluie <ryouya.nakai0103@gmail.com>
2019-11-28 02:53:52 +0900

    リポジトリの作成

---
+++ KADAI01/ConsoleApp1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+
+
+
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("5x^2+3x+1");//問題
+            int X = int.Parse(Console.ReadLine());//intに変換して入力
+            Console.WriteLine(TEST(X));//関数呼び出しと表示
+
+        }
+        static int TEST(int XX)
+        {
+            int AN;//答え保存
+            var NN = Math.Pow(XX, 2);
+            var N = Convert.ToInt32(NN);
+            AN = 5 * (N) + (3 * XX) + 1;//計算式
+
+            return AN;
+        }
+    }
+}
---
+++ KADAI02/KADAI02/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+
+
+
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("5x^2+3x+1");//問題
+            Console.WriteLine("(a^x+b)^x+cの場合のXを入力");//問題
+            int X = int.Parse(Console.ReadLine());//intに変換して入力
+
+            Console.WriteLine("(a^x+b)^x+cの場合のaを入力");//問題
+            int a = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("(a^x+b)^x+cの場合のbを入力");//問題
+            int b = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("(a^x+b)^x+cの場合のcを入力");//問題
+            int c = int.Parse(Console.ReadLine());
+
+
+            Console.WriteLine(TEST(X, a, b, c));//関数呼び出しと表示
+
+        }
+        static int TEST(int X, int a, int b, int c)
+        {
+            int AN;//答え保存
+            var NN = Math.Pow(X, 2);
+            var N = Convert.ToInt32(NN);
+            AN = X * (a * X + b) + c;//計算式
+
+            return AN;
+        }
+
+        static int TEST2(int X, int a, int b, int c)
+        {
+            int AN;//答え保存
+            var NN = Math.Pow(X, 2);
+            var N = Convert.ToInt32(NN);
+            AN = X * (a * X + b) + c;//計算式
+
+            return AN;
+        }
+    }
+}
